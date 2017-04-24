fc8c451285bff3519f0cab25ef6017ca3d419d95
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-24 18:11:43 +0100

    initial data and folder setup

---
+++ server/seed/data/bins.js
@@ -0,0 +1,79 @@
+{
+    Manchester: {
+        plastic: 'brown',
+        paper: 'blue',
+        metal: 'brown',
+        glass: 'brown',
+        garden: 'green',
+        nonrecyclable: 'grey'
+    },
+    Bolton: {
+        plastic:  'burgundy',
+        paper:  'beige',
+        metal:  'burgundy',
+        glass: 'burgundy',
+        garden: 'green',
+        non-recyclable: 'grey'
+    },
+    Salford: {
+        plastic:  'brown',
+        paper:  'blue',
+        metal:  'brown',
+        glass: 'brown',
+        garden: 'pink',
+        non-recyclable: 'black'
+    },
+    Wigan: {
+        plastic:  'brown',
+        paper:  'blue',
+        metal:  'brown',
+        glass: 'brown',
+        garden: 'green',
+        non-recyclable: 'black'
+    },
+    Bury: {
+        plastic:  'blue',
+        paper:  'green',
+        metal:  'blue',
+        glass: 'blue',
+        garden: 'brown',
+        non-recyclable: 'grey'
+    },
+    Oldham: {
+        plastic:  'brown',
+        paper:  'blue',
+        metal:  'brown',
+        glass: 'brown',
+        garden: 'green',
+        non-recyclable: 'grey'
+    },
+    Trafford: {
+        plastic:  'black',
+        paper:  'blue',
+        metal:  'black',
+        glass: 'black',
+        garden: 'green',
+        non-recyclable: 'grey'
+    },
+    Tameside: {
+        plastic:  'black',
+        paper:  'blue',
+        metal:  'black',
+        glass: 'black',
+         garden: 'brown',
+        non-recyclable: 'green'
+    },
+    Stockport: {
+        plastic:  'brown',
+        paper:  'blue',
+        metal:  'brown',
+        glass: 'brown',
+        garden: 'green',
+        non-recyclable: 'black'
+    },
+}
+
+
+// GET api/:council/bins
+// GET api/:council/collection
+// GET api/recyclingcentres
---
+++ server/seed/data/recycling-centres.js
@@ -0,0 +1,52 @@
+[
+  {
+    name: 'Longley Lane Recycling Centre',
+    address: 'Longley Lane Recycling Centre, Longley Lane, Sharston, M22 4RQ',
+    postcode: 'M22 4RQ'
+  },
+  {
+    name: 'Sandfold Lane Recycling Centre',
+    address: 'Sandfold Lane Recycling Centre, Sandfold Lane, Manchester, M19 3BJ',
+    postcode: 'M19 3BJ'
+  },
+  {
+    name: 'Reliance Street Recycling Centre',
+    address: 'Reliance Street Recycling Centre, Reliance Street, Manchester, M40 3EZ',
+    postcode: 'M40 3EZ'
+  },
+  {
+    name: 'Chester Road Recycling Centre',
+    address: 'Chester Road Recycling Centre, Chester Road, Stretford, M32 9AU',
+    postcode: 'M32 9AU'
+  },
+  {
+    name: 'Ash Road Recycling Centre',
+    address: 'Ash Road Recycling Centre, Ash Road, Droylsden, M43 6QU',
+    postcode: 'M43 6QU'
+  },
+  {
+    name: 'Lumns Lane Recycling Centre',
+    address: 'Lumns Lane Recycling Centre, Lumns Lane, Clifton, M27 8LN',
+    postcode: 'M27 8LN'
+  },
+  {
+    name: 'Liverpool Road Recycling Centre',
+    address: 'Liverpool Road Recycling Centre, Liverpool Road, Eccles, M30 7RH',
+    postcode: 'M30 7RH'
+  },
+  {
+    name: 'Cobden Street Recycling Centre',
+    address: 'Cobden Street Recycling Centre, Cobden Street, Salford, M6 6NA',
+    postcode: 'M6 6NA'
+  },
+  {
+    name: 'Spring Vale Recycling Centre',
+    address: 'Spring Vale Recycling Centre, Spring Vale, Middleton, M24 2HR',
+    postcode: 'M24 2HR'
+  },
+  {
+    name: 'Cemetery Road Recycling Centre',
+    address: 'Cemetery Road Recycling Centre, Cemetery Road, Radcliffe, M26 4FT',
+    postcode: 'M26 4FT'
+  }
+]
