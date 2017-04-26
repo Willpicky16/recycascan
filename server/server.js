8c5c06b842b398c6366f4899b650f5f261604f62
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-26 15:33:38 +0100

    [feat-2] added mlabs database info

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ const express = require ('express');
 const app = express();
 const apiRouter = require('./routes/api');
 const mongoose = require('mongoose');
+var config = require('./config');
+var db = 'mongodb://user:password@ds055915.mlab.com:55915/recycascan';
+var PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
 
-mongoose.connect('mongodb://localhost/recycascan');
+mongoose.Promise = global.Promise;
+mongoose.connect(db, function (err) {
+  if (!err) {
+    console.log(`connected to the Database: ${db}`);
+  } else {
+    console.log(`error connecting to the Database ${err}`);
+  }
+});
 
 app.use(function (request, response, next) {
     console.log('Received request!');
@@ -16,9 +26,9 @@ app.use('/*', function (request, response) {
     response.status(404).send({reason: '404 -ROUTE NOT FOUND'});
 });
 
-app.listen(3000, function (error) {
+app.listen(PORT, function (error) {
     if (error) {
         return console.log(error);
     }
-    console.log('App listening on port 3000');
+    console.log(`listening on port ${PORT}`);
 });
