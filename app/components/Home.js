90147ab35314c46cc8462fad7c78fe6063c49798
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-27 12:50:30 +0100

    [feat-13] implemented home page form to convert postcode to council

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-import PostcodeFormView from './forms/PostcodeForm';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Form, InputField } from 'react-native-form-generator';
+import { postcodeToCouncil } from '../helpers/postcodeToCouncil';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  
+  handleFormChange(data) {
+    this.setState({
+      postcode: data.postcode
+    });
+  }
+  onButtonPress() {
+    postcodeToCouncil(this.state.postcode)
+  }
   render () {
     return (
       <View style={styles.container}>
-        <PostcodeFormView />
+        <Text>ADD NEW PRODUCT:</Text>
+        <Form label="Add new product form" ref="form" onChange={this.handleFormChange.bind(this)}>
+          <InputField ref="postcode" placeholder="Postcode" helpText="Enter your postcode" />
+        </Form>
+        <Button title="Submit" onPress={this.onButtonPress.bind(this)} color="#841584" accessibilityLabel="Submit"/>
       </View>
     )
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'flex-start'
+    justifyContent: "center",
+    alignItems: "center"
   }
-})
+});
