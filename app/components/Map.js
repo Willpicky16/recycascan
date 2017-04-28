8c9b8a2ef127d367403f3bdc568880797c84c12f
Sarah Jane Morris <apmorris83@gmail.com>
2017-04-28 12:48:10 +0100

    [feat-24] react-native-maps settings added, and map rendering a marker

--- app/components/Map.js
+++ app/components/Map.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default class Map extends Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [{
+        title: 'Longley Lane',
+        latitude: 53.3977763,
+        longitude: -2.248374799999965
+      }]
+    }
+  }
+  render() {
     return (
       <View style={styles.container}>
-        <Text>Map</Text>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 53.3977763,
+            longitude: -2.248374799999965,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5,
+          }}
+        >
+          {this.state.markers.map((marker, i) => {
+            return (
+              <MapView.Marker
+                key={i}
+                coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+                title={marker.title}
+              />
+            );
+          })}
+
+        </MapView>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'flex-start'
-  }
-})
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
