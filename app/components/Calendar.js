6737d2999a74ccc90a981edb7f3e027f3b713a6b
William Pickering <willpicky@gmail.com>
2017-04-28 17:43:36 +0100

    [feat-6] calendar tab now shows collection data

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, AsyncStorage } from "react-native";
+import { StyleSheet, Text, View, ScrollView, AsyncStorage } from "react-native";
+import axios from 'axios';
 
 export default class Calendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      collections: [],
       userDetails: {},
       detailsRecieved: false
     };
+    this.getCollections = this.getCollections.bind(this);
   }
 
   componentDidMount() {
@@ -17,13 +20,42 @@ export default class Calendar extends Component {
         userDetails: val,
         detailsRecieved: true
       });
+      this.getCollections(val.council);
     });
+  }
+
+  getCollections (council) {
+    axios
+      .get(`https://vast-eyrie-43528.herokuapp.com/api/collections?council=${council}`)
+      .then((res) => {
+        this.setState({
+          collections: res.data.collections
+        });
+      })
+      .catch((err) => {
+        alert(err);
+      });
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Calendar</Text>
+        <View style={styles.calendarContainer}>
+          <ScrollView>
+            {this.state.collections.map((collection, key) => {
+                return (
+                  <View key={key}>
+                    <Text style={styles.date}>
+                      {collection.day.toString()} {collection.month.toString()} {collection.year.toString()} ({collection.council.toString()})
+                    </Text>
+                    <Text>
+                      {collection.bins.toString()}
+                    </Text>
+                  </View>
+                )
+              })}
+          </ScrollView>
+        </View>
       </View>
     );
   }
@@ -32,8 +64,15 @@ export default class Calendar extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    justifyContent: "center"
+  },
+  calendarContainer: {
+    flex: 1,
     justifyContent: "center",
-    alignItems: "flex-start"
+    alignItems: "center"
+  },
+  data: {
+    paddingBottom: 10,
+    fontSize: 20
   }
 });
