abd2c7e6b2e2d062f1719a11e568bff248bb7478
William Pickering <willpicky@gmail.com>
2017-05-02 11:12:12 +0100

    [feat-6] Bin images now showing in calendar section

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, ScrollView, AsyncStorage } from "react-native";
+import { StyleSheet, Text, View, ScrollView, AsyncStorage, Image } from "react-native";
 import axios from 'axios';
+import moment from 'moment';
+
+let day = moment().format('D')
+let month = moment().format('MMMM');
+let year = moment().format('YYYY');
+console.log(`${day} ${month} ${year}`);
 
 export default class Calendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       collections: [],
-      userDetails: {},
-      detailsRecieved: false
+      userDetails: {}
     };
     this.getCollections = this.getCollections.bind(this);
   }
@@ -17,8 +22,7 @@ export default class Calendar extends Component {
     AsyncStorage.getItem('userDetails', (err, result) => {
       let val = JSON.parse(result);
       this.setState({
-        userDetails: val,
-        detailsRecieved: true
+        userDetails: val
       });
       this.getCollections(val.council);
     });
@@ -31,6 +35,7 @@ export default class Calendar extends Component {
         this.setState({
           collections: res.data.collections
         });
+        console.log(this.state.collections[5].bins[2]);
       })
       .catch((err) => {
         alert(err);
@@ -38,41 +43,64 @@ export default class Calendar extends Component {
   }
 
   render() {
+    if (this.state.userDetails === null) return (
+      <View style={styles.containerError}>
+        <Text style={styles.titleError}>Please enter a postcode</Text>
+      </View>
+    )
     return (
-      <View style={styles.container}>
-        <View style={styles.calendarContainer}>
-          <ScrollView>
+      <ScrollView>
+        <View style={styles.container}>
             {this.state.collections.map((collection, key) => {
                 return (
                   <View key={key}>
                     <Text style={styles.date}>
                       {collection.day.toString()} {collection.month.toString()} {collection.year.toString()} ({collection.council.toString()})
                     </Text>
-                    <Text>
-                      {collection.bins.toString()}
-                    </Text>
+                    {collection.bins.map((bin, i) => {
+                      return (
+                        <View style={styles.imageView}>
+                          <Image style={styles.binImage} source={{ uri: bin }}/>
+                        </View>
+                      )
+                    })}
                   </View>
                 )
               })}
-          </ScrollView>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  calendarContainer: {
+  containerError: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
   },
-  data: {
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  date: {
+    paddingTop: 50,
     paddingBottom: 10,
     fontSize: 20
+  },
+  titleError: {
+    fontSize: 20,
+    color: 'red'
+  },
+  bins: {
+    color: 'blue'
+  },
+  binImage: {
+    height: 100,
+    width: 65
+  },
+  imageView: {
+    flexDirection: 'row'
   }
 });
