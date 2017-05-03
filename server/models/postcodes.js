db0292b77f821157be7a2083ff7448c9e8036ce0
William Pickering <willpicky@gmail.com>
2017-05-03 09:32:53 +0100

    [clean-up] server file removed

--- server/models/postcodes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const postcodeSchema = new Schema({
-    postcode: String,
-    council: String,
-});
-
-module.exports = mongoose.model('postcodes', postcodeSchema);
